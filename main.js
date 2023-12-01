50b68b93dcd49ca19a91336c0d230e2b0cf9dab6
allan-leite <allan.leite2004@gmail.com>
2023-12-01 10:10:35 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        
+        const nomeTarefa = $('#nome-tarefa').val();
+        const novoItem = $('<ul></ul>')
+        var lista = $(`<li>${nomeTarefa}</li>`).appendTo(novoItem);
+        $(novoItem).appendTo('.container')
+        
+        $('ul').on('click', 'li', function(){
+            lista = $('li').css({"text-decoration":"line-through"})
+        })
+    })
+})
